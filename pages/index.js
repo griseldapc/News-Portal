92773aa8016a159c38bdb22a2040e44b6d1e5fca
Fadhila Zalfa Alisya <101534228+zalfaa22@users.noreply.github.com>
2023-03-27 14:59:27 +0700

    Update

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,9 @@
-import NavBar from '../components/NavBar';
-import Footer from '../components/Footer';
-import Card from '../components/Card';
-import Contact from '../components/contact';
+import Display from "../components/Display";
 
 export default function Home() {
   return (
     <div>
-      <NavBar />
-      <Card />
-      <Contact />
-      <Footer />
+      <Display />
     </div>
   );
 }
